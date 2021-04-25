1b5bc891583ba431840860a8f92ce49ba3d257d0
Venky Tumkur <venky.tumkur@mobiledgex.com>
2021-04-26 00:41:26 +0200

    Goroutines for URL cleanup

--- twitter-to-rss.go
+++ twitter-to-rss.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	"os"
 	"regexp"
+	"sort"
+	"sync"
 	"time"
 
 	"github.com/dghubble/go-twitter/twitter"
@@ -16,43 +18,118 @@ import (
 
 const (
 	BearerTokenEnv  = "TWITTER_BEARER_TOKEN"
+	FeedUrlEnv      = "FEED_URL"
 	FeedTitle       = "Atlas Obscura"
-	FeedUrl         = "https://example.com" // FIXME
 	FeedDescription = "Atlas Obscura Tweets"
 	FeedAuthor      = "Venky"
 	FeedAuthorEmail = "venkytv@gmail.com"
 )
 
 type FeedItem struct {
-	Title   string
-	Url     string
-	Created time.Time
+	title   string
+	url     string
+	created time.Time
 }
 
-func genFeed(items []FeedItem) *feeds.Feed {
+type FeedUrl string
+
+var utm_re = regexp.MustCompile(`\?utm_.*$`)
+
+func genFeed(items []FeedItem, url FeedUrl) *feeds.Feed {
 	feed := &feeds.Feed{
 		Title:       FeedTitle,
-		Link:        &feeds.Link{Href: FeedUrl},
+		Link:        &feeds.Link{Href: string(url)},
 		Description: FeedDescription,
 		Author:      &feeds.Author{Name: FeedAuthor, Email: FeedAuthorEmail},
 		Created:     time.Now(),
 	}
 	for _, item := range items {
 		feed.Add(&feeds.Item{
-			Title:   item.Title,
-			Link:    &feeds.Link{Href: item.Url},
-			Created: item.Created,
+			Title:   item.title,
+			Link:    &feeds.Link{Href: item.url},
+			Created: item.created,
 		})
 	}
 
 	return feed
 }
 
+func gen(items []FeedItem) <-chan FeedItem {
+	out := make(chan FeedItem)
+	go func() {
+		for _, item := range items {
+			out <- item
+		}
+		close(out)
+	}()
+	return out
+}
+
+type result struct {
+	item FeedItem
+	err  error
+}
+
+func fixer(done <-chan struct{}, items <-chan FeedItem, c chan<- result) {
+	for item := range items {
+		resp, err := http.Get(item.url)
+		if err == nil {
+			url := utm_re.ReplaceAllString(
+				resp.Request.URL.String(), "")
+			item.url = url
+		}
+		select {
+		case c <- result{item, err}:
+		case <-done:
+			return
+		}
+	}
+}
+
+func fixAllUrls(items []FeedItem) ([]FeedItem, error) {
+	// Set up a done channel to signal completion
+	done := make(chan struct{})
+	defer close(done)
+
+	items_chan := gen(items)
+
+	// Start a fixed number of channels to fix URLs
+	c := make(chan result)
+	var wg sync.WaitGroup
+	const numFixers = 10
+	wg.Add(numFixers)
+	for i := 0; i < numFixers; i++ {
+		go func() {
+			fixer(done, items_chan, c)
+			wg.Done()
+		}()
+	}
+	go func() {
+		wg.Wait()
+		close(c)
+	}()
+
+	out := make([]FeedItem, 0)
+	for r := range c {
+		if r.err != nil {
+			return nil, r.err
+		}
+		out = append(out, r.item)
+	}
+
+	return out, nil
+}
+
 func main() {
 	ctx := context.Background()
 	token, ok := os.LookupEnv(BearerTokenEnv)
 	if !ok {
-		log.Fatalf("Env var not set: %s", BearerTokenEnv)
+		log.Fatal("Env var not set: ", BearerTokenEnv)
+	}
+
+	feedUrl, ok := os.LookupEnv(FeedUrlEnv)
+	if !ok {
+		log.Fatal("Env var not set: ", FeedUrlEnv)
 	}
 
 	ts := oauth2.StaticTokenSource(
@@ -67,7 +144,6 @@ func main() {
 	}
 
 	tweet_re := regexp.MustCompile(`(.*?)\s(https?://.*)`)
-	utm_re := regexp.MustCompile(`\?utm_.*$`)
 
 	tweets, _, err := client.Timelines.UserTimeline(
 		&twitter.UserTimelineParams{ScreenName: "atlasobscura"})
@@ -82,26 +158,24 @@ func main() {
 		t := tweet_re.FindStringSubmatch(tweet)
 		text := t[1]
 		url := t[2]
-
-		resp, err := http.Get(url)
-		if err != nil {
-			log.Fatal("http.Get => %v", err.Error())
-		}
-		finalUrl := utm_re.ReplaceAllString(
-			resp.Request.URL.String(), "")
-		fmt.Printf("%s %s\n", text, finalUrl)
-
 		createdAt, err := message.CreatedAtTime()
 		if err != nil {
 			log.Fatal(err)
 		}
 		feedItems = append(feedItems, FeedItem{
-			Title:   text,
-			Url:     finalUrl,
-			Created: createdAt,
+			title:   text,
+			url:     url,
+			created: createdAt,
 		})
 	}
 
-	feed := genFeed(feedItems)
+	feedItems, err = fixAllUrls(feedItems)
+	if err != nil {
+		log.Fatal(err)
+	}
+	sort.Slice(feedItems, func(i, j int) bool {
+		return feedItems[i].created.After(feedItems[j].created)
+	})
+	feed := genFeed(feedItems, FeedUrl(feedUrl))
 	fmt.Println(feed.ToAtom())
 }
