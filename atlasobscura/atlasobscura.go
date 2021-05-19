2bf77b4c8d27d3d1d0de0dd9dae70aa81caf378b
Venky Tumkur <venky.tumkur@mobiledgex.com>
2021-05-20 00:00:19 +0200

    Remove dead code

--- atlasobscura/atlasobscura.go
+++ atlasobscura/atlasobscura.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -154,11 +153,6 @@ func cacheFeed() {
 	)
 	tc := oauth2.NewClient(ctx, ts)
 	client := twitter.NewClient(tc)
-
-	demux := twitter.NewSwitchDemux()
-	demux.Tweet = func(tweet *twitter.Tweet) {
-		fmt.Println(tweet.Text)
-	}
 
 	tweet_re := regexp.MustCompile(`(.*?)\s(https?://.*)`)
 
